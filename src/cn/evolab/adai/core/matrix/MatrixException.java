6e1700d84d8bcf7ca664e6510916d5392ba90a40
Lin Lin <lin@evolab.cn>
2015-01-06 10:56:09 +0800

    update Matrix

---
+++ src/cn/evolab/adai/core/matrix/MatrixException.java
@@ -0,0 +1,10 @@
+package cn.evolab.adai.core.matrix;
+
+public class MatrixException extends Exception {
+	private static final long serialVersionUID = 1L;
+	public MatrixException(String string) {
+		// TODO Auto-generated constructor stub
+		super(string);
+	}
+	
+}
